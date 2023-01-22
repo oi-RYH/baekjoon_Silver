6033506140deb8e2bdf9a53ff0fbfc2ead3a00e7
oi-RYH <ryuyaelhwa@gmail.com>
2023-01-22 21:54:55 +0900

    실패

--- 1251/Silver1251.c
+++ 1251/Silver1251.c
@@ -1,42 +1,83 @@
 // 1251
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-char text[55];
+char text[51];
+char result[20825][51];
+int cut[2];
+int cnt;
+int len;
+
+void resultString(char *text, char (*result)[51], int *cut);
+void pick(void);
+int nCr();
 
 int main () {
 
     scanf("%s", text);
+    len = strlen(text);
 
-    int min[2] = {0, };
-    char temp;
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = min[i] + 1; j < strlen(text) - (2 - i); j++) {
-            if (text[min[i]] > text[j]) {
-                min[i] = j;
-                if (text[j] == 'a') {
-                    break;
-                }
-            }
-        }
-        if (i == 0) {
-            min[i + 1] = min[i];
-            for (int k = 0; k < (min[i] + 1) / 2; k++) {
-                temp = text[k];
-                text[k] = text[min[i]];
-                text[min[i]] = temp;
-            } 
-        } else {
-            for (int k = 0; k < (min[i] - min[i - 1]) / 2; k++) {
-                temp = text[k + min[i - 1] + 1];
-                text[k + min[i - 1] + 1] = text[min[i] - k];
-                text[min[i] - k] = temp;
-            } 
+    for (int i = 0; i < len - 2; i++) {
+        for (int j = i + 1; j < len - 1; j++) {
+            cut[0] = i;
+            cut[1] = j;
+            resultString(text, result, cut);
         }
     }
 
-    printf("%s", text);
+    pick();
+
+    printf("%s", result[0]);
 
     return 0;
+}
+void resultString(char *text, char (*result)[51], int *cut) {
+
+    int strcnt = 0;
+
+    for (int i = 0; i < cut[0] + 1; i++) {
+        result[cnt][strcnt] = text[cut[0] - i];
+        strcnt++;
+    }
+
+    for (int i = 0; i < cut[1] - cut[0]; i++) {
+        result[cnt][strcnt] = text[cut[1] - i];
+        strcnt++;
+    }
+
+    for (int i = 0; i < len - 1 - cut[1]; i++) {   
+        result[cnt][strcnt] = text[len - 1 - i];
+        strcnt++;
+    }
+
+    cnt++;
+
+}
+
+void pick() {
+    
+    char temp[51] = {0, };
+    for (int i = 0; i < nCr() - 1; i++) {
+        for (int j = i + 1; j < nCr() - 1; j++) {
+            if (strcmp(result[i], result[j]) > 0) {
+                strcpy(temp, result[i]);	
+			    strcpy(result[i], result[j]);
+			    strcpy(result[j], temp);
+            }
+        }
+    }
+}
+
+int nCr() {
+
+    int C[20825] = {0, };
+    C[2] = 1;
+    C[3] = 3;
+
+    for (int i = 3; i < 20825; i++) {
+        C[i] = C[i - 1] + i - 1;
+    }
+
+    return C[len];
 }
