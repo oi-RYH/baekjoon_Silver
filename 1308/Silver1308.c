ca6c2f21c6fa67e104b66af9409fbae05bf2fdaf
oi-RYH <ryuyaelhwa@gmail.com>
2023-04-10 14:30:26 +0900

    1000년 차이 계산하지 않았음.

--- 1308/Silver1308.c
+++ 1308/Silver1308.c
@@ -1,44 +1,83 @@
 // 1308
 #include <stdio.h>
 
-int main () {
+int isLeapYear(int year);
 
-    int YMD[2][3] = {0, };
-    int flag[2] = {0, };
-    int day[2] = {0, };
-    int month[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int Ymonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int main() {
 
-    scanf("%d %d %d", &YMD[0][0], &YMD[0][1], &YMD[0][2]);
-    scanf("%d %d %d", &YMD[1][0], &YMD[1][1], &YMD[1][2]);
+    int start[3];
+    int finish[3];
 
-    for (int i = 0; i < 1; i++) {
-        if ((YMD[i][0] % 4 == 0) && (YMD[i][0] % 100 != 0) && (YMD[i][0] % 400 == 0)) {
-            flag[i] = 1;
+    int startSum = 0;
+    int finishSum = 0;
+
+    scanf("%d %d %d", &start[0], &start[1], &start[2]);
+    scanf("%d %d %d", &finish[0], &finish[1], &finish[2]);
+
+
+
+    for (int i = 0; i < start[0]; i++) { // 연도 일수 덧셈
+        if (isLeapYear(i)) {
+            startSum += 366;
+        } else {
+            startSum += 365;
         }
     }
 
-    for (int i = 0; i < 2; i++) {
-        if (flag[i]) {
-            day[i] = YMD[i][0] * 364 - YMD[i][2];
-            for (int j = 0; j < YMD[i][1]; j++) {
-                day[i] += Ymonth[j];
+    for (int i = 1; i < start[1]; i++) { // 월 일수 덧셈
+        if ((i == 4) || (i == 6) || (i == 9) || (i == 11)) {
+            startSum += 30;
+        } else if (i == 2) {
+            if (isLeapYear(start[0])) {
+                startSum += 29;
+            } else {
+                startSum += 28;
             }
         } else {
-            day[i] = YMD[i][0] * 365 - YMD[i][2];
-            for (int j = 0; j <= YMD[i][1]; j++) {
-                day[i] += month[j];
-            }
+            startSum += 31;
+        }
+    }
+    
+    startSum += start[2]; // 일 덧셈
+    
+
+
+    for (int i = 0; i < finish[0]; i++) { // 연도 일수 덧셈
+        if (isLeapYear(i)) {
+            finishSum += 366;
+        } else {
+            finishSum += 365;
         }
     }
 
-    //242
-
-    if ((day[1] - day[0]) < (364 * 242 + 365 * 758)) {
-        printf("D-%d", day[1] - day[0]);
-    } else {
-        printf("gg");
+    for (int i = 1; i < finish[1]; i ++) { // 월 일수 덧셈
+        if ((i == 4) || (i == 6) || (i == 9) || (i == 11)) {
+            finishSum += 30;
+        } else if (i == 2) {
+            if (isLeapYear(finish[0])) {
+                finishSum += 29;
+            } else {
+                finishSum += 28;
+            }
+        } else {
+            finishSum += 31;
+        }
     }
+    
+    finishSum += finish[2]; // 일 덧셈
+    
+
+    printf("D-%d", finishSum - startSum);
+
 
     return 0;
+}
+
+int isLeapYear(int year) { // 윤년이면 1, 아니면 0 리턴 
+
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
